faab3196395f18c443c3460834276692d2a402e6
mdemong87 <mdemong87@gmail.com>
2024-09-22 00:54:51 +0600

    update navber and improvement the site design

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let header = document.querySelector("#header");
 
 window.addEventListener('scroll', (e) => {
     const hight = scrollY;
-    if (hight > 150) {
+    if (hight > 80) {
         header.style.background = "white";
         header.style.borderBottom = "1px solid #E4EAF8";
         header.style.transition = "background .6s";
@@ -27,3 +27,32 @@ window.addEventListener('scroll', (e) => {
         header.style.borderBottom = "none";
     }
 });
+
+
+//navber for small screen management
+let navmenu = document.querySelector("#navmenu");
+let smallnav = document.querySelector('#smallnav');
+let openspace = document.querySelector("#openspace");
+let crossiconsnav = document.querySelector("#crossiconsnav");
+
+
+navmenu.addEventListener("click", () => {
+    smallnav.style.display = "block";
+    // smallnav.classList.add("shownav");
+});
+
+openspace.addEventListener("click", () => {
+    smallnav.style.display = "none";
+})
+
+crossiconsnav.addEventListener("click", () => {
+    smallnav.style.display = "none";
+})
+
+
+window.addEventListener("resize", (e) => {
+    const width = window.innerWidth;
+    if (width > 1024) {
+        smallnav.style.display = "none";
+    }
+})
